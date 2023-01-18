a52ccbc62197680085df84c157604f83ad1e688c
Humberto Benedito <humbertobenedito@gmail.com>
2023-01-18 15:14:39 +0100

    add last changes

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ const {API_URL_MOCK_DATA} = process.env
 
 export const getNormalRange = async () => {
   try {
-    const response = await fetch(`${API_URL_MOCK_DATA}/normalRanges`)
+    const response = await fetch(`${API_URL_MOCK_DATA || 'http://demo3373948.mockable.io/'}/normalRanges`)
     return await response.json()
   } catch (error) {
     return error
@@ -11,7 +11,7 @@ export const getNormalRange = async () => {
 
 export const getFixedRange = async () => {
   try {
-    const response = await fetch(`${API_URL_MOCK_DATA}/fixedRanges`)
+    const response = await fetch(`${API_URL_MOCK_DATA || 'http://demo3373948.mockable.io/'}/fixedRanges`)
     return await response.json()
   } catch (error) {
     return error
